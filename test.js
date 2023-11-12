997f9e3920ef14590da3e4d19f1cc13d2590c3e5
Erdem Ayaz <erdemayaz@Erdems-MacBook-Pro.local>
2023-11-12 21:21:02 +0300

    Added beauty standards

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
-const assert = require('node:assert');
-const GeObjectId = require('./index.js');
+const assert = require('node:assert')
+const GeObjectId = require('./index.js')
 
-const LAT = 41.014399;
-const LON = 28.9855536;
-const RAD = 21;
+const LAT = 41.014399
+const LON = 28.9855536
+const RAD = 21
 
-const objectId = GeObjectId.encodePoint(LAT, LON, RAD);
+const objectId = GeObjectId.encodePoint(LAT, LON, RAD)
 
 assert.strictEqual(typeof objectId, 'string')
 assert.strictEqual(objectId.length, 24)
 assert(objectId.match(/^[0-9a-f]{24}$/))
 
-const { lat, lon, rad } = GeObjectId.decodePoint(objectId);
+const { lat, lon, rad } = GeObjectId.decodePoint(objectId)
 
-assert.strictEqual(lat, LAT);
-assert.strictEqual(lon, LON);
-assert.strictEqual(rad, RAD);
+assert.strictEqual(lat, LAT)
+assert.strictEqual(lon, LON)
+assert.strictEqual(rad, RAD)
